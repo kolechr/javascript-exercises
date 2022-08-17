1d785033d7572cda428b4e66eb2de46bb12930e7
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 21:42:05 +0900

    Pass ftoc() tests for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  const celcius = Math.round(((fah - 32) * (5 / 9)) * 10) / 10;
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const fahrenheit = Math.round(((cel * (9 / 5)) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
