af6d7368e49dfbf14d1485f56c4824140da83682
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 18:07:34 +0900

    Pass 3 tests for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    // Declare variable to store sum
+    let sum = 0;
+    // If numTwo less than numOne, then start fron numTwo
+    if (numTwo < numOne) {
+        // Loop to increment the sum by i until it's reach numTwo
+        for (let i = numTwo; i <= numOne; i++) {
+            sum += i;
+        }
+    }
+    // Else, loop to increment the sum by i until it's reach numTwo
+    for (let i = numOne; i <= numTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
