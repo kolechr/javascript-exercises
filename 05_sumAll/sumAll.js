d9f6d258c378df6082622545cc9a64418301da91
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 18:19:19 +0900

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,26 @@
 const sumAll = function(numOne, numTwo) {
     // Declare variable to store sum
     let sum = 0;
-    // If numTwo less than numOne, then start fron numTwo
-    if (numTwo < numOne) {
-        // Loop to increment the sum by i until it's reach numTwo
-        for (let i = numTwo; i <= numOne; i++) {
+    // If one or both of args are negative, then return 'ERROR'
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR'
+    }
+    // If one or both of args are non a number, then return 'ERROR'
+    else if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return 'ERROR'
+    }
+    else {
+        // If numTwo less than numOne, then start fron numTwo
+        if (numTwo < numOne) {
+            // Loop to increment the sum by i until it's reach numTwo
+            for (let i = numTwo; i <= numOne; i++) {
+                sum += i;
+            }
+        }
+        // Else, loop to increment the sum by i until it's reach numTwo
+        for (let i = numOne; i <= numTwo; i++) {
             sum += i;
         }
-    }
-    // Else, loop to increment the sum by i until it's reach numTwo
-    for (let i = numOne; i <= numTwo; i++) {
-        sum += i;
     }
     return sum;
 };
