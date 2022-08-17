fbf39dc3d56a8b231795c42e433aeea0bbfcc686
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 17:51:00 +0900

    Finish 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,9 @@
 const removeFromArray = function(numArr, ...targets) {
     // Convert target values into array-like
     const targetArr = [...targets];
-    // Loop through the number array
-        // Loop through the target array to remove matched value
+    // Loop through the target array to remove matched value
     for (const target of targets) {
+        // Loop through the number array to compare both values
         for (let i = 0; i < numArr.length; i++) {
             // If both values are the same then remove from the list
             if (numArr[i] === target) {
