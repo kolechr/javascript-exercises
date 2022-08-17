670fd52d3774967804d11068fe1e0fa6e7edaa6e
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 18:30:16 +0900

    Pass first test for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check if the year is divisible by 4 && only 400
+    if (year % 4 !== 0 && year % 400 !== 0) {
+        return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
