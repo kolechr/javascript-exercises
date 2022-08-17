06b7765595e398057fd4abaeb634305a0765370b
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 16:02:43 +0900

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,12 @@
-const reverseString = function(word) {
-    //Split string into an array
-    //Reverse the array
-    //Join the elements
+const reverseString = function(str) {
+    // Split string into an array
+    const charArray = str.split('');
+    // Reverse the array
+    const reversedChar = charArray.reverse();
+    // Join the elements
+    const reversedString = reversedChar.join('');
+    // Return the reversed string
+    return reversedString;
 };
 
 // Do not edit below this line
