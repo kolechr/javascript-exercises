44afc9fd27419295b45c83d9ffceb6c6c489550f
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-17 17:08:49 +0900

    Pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numArr, target) {
+    // Convert target values into array-like
+    // Loop through the target array
+    for (let i = 0; i < numArr.length; i++) {
+        // If both values are the same then remove from the list
+        if (numArr[i] === target) {
+                numArr.splice(i, 1);
+        }
+    }
+    return numArr;
 };
 
 // Do not edit below this line
