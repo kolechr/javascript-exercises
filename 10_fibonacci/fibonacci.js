4ca382482c9d8e2123f80f21b969c5ca3550c3f8
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-26 14:32:40 +0900

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
+    // let a = 0;
+    // let b = 1;
+    // for (let i = 1; i < num; i++) {
+    //     const c = b;
+    //     b = a + b;
+    //     a = c;
+    // }
+    // return b;
+    let prev = 1;
+    let curr = 1;
+    for (let i = 2; i < num; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
