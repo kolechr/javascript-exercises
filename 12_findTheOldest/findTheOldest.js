994322e121c5af94bdb8a10b5ba8143b4e5f5834
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-26 15:25:19 +0900

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sort = people.sort((firstPerson, secondPerson) => 
+    age(firstPerson.yearOfDeath, firstPerson.yearOfBirth) 
+    > age(secondPerson.yearOfDeath, secondPerson.yearOfBirth) 
+    ? -1 : 1);
 
-};
+    return sort[0];
+    };
+
+function age(death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
