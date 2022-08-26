85d25c64098d671ac3e6fc1fd36dfa9007b95e21
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-26 12:04:11 +0900

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	const totalSum = nums.reduce((total, num) => {
+    return total += num;
+  }, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  const totalProduct = nums.reduce((total, num) => {
+    return total *= num;
+  }, 1);
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  if (num !== 0) {
+    for (let i = num; i > 0; i--) {
+      factorial *= i;
+    }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
