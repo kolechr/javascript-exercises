53bb5096b0f51a5fde2943dfb0b74455ef1539d5
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-26 13:50:46 +0900

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    const reversedChar = newStr.split("").reverse().join("");
+    if (newStr === reversedChar) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
